773e0e857de352fd6934ec4d3bb898123db77651
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-11 22:22:32 +0300

    Initial commit

---
+++ CenturiestoMinutes/Program.cs
@@ -0,0 +1,23 @@
+﻿class CenturiestoMinutes
+{
+    public static void Main(string[] args)
+    {
+        int century = int.Parse(Console.ReadLine());
+            
+        int centuryInYears = 100;
+        double daysInyear = 365.2422;
+       int hoursInday = 24;
+        int minutesInHour = 60;
+
+
+        int years = century * centuryInYears;
+        int days = (int) (years * daysInyear);
+        int hours = hoursInday *(int) days;
+        int minutes = minutesInHour * hours;
+        
+        Console.WriteLine($"{century} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+        
+        
+
+    }
+}
